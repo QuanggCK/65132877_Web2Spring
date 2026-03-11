cff58039866382842a760e4a293847dc9e8e77e7
QuanggCK <zaphkiel1610@gmail.com>
2026-03-11 15:16:06 +0700

    update Post.java

--- ViDuGiuaKy/src/main/java/com/example/demo/controllers/Post.java
+++ ViDuGiuaKy/src/main/java/com/example/demo/controllers/Post.java
@@ -1,5 +1,47 @@
 package com.example.demo.controllers;
 
 public class Post {
+	public String id;
+    public String title;
+    public String content;
+    public String categoryId;
 
+    public Post(String id, String title, String content, String categoryId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.categoryId = categoryId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
 }
