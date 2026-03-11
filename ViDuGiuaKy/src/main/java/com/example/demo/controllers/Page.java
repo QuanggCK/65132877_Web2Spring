b6037d2c2314eb820d1fb70541f220b47b76e85c
QuanggCK <zaphkiel1610@gmail.com>
2026-03-11 15:12:39 +0700

    add Page.java

---
+++ ViDuGiuaKy/src/main/java/com/example/demo/controllers/Page.java
@@ -0,0 +1,57 @@
+package com.example.demo.controllers;
+
+public class Page {
+	private String id;
+    private String pageName;
+    public String keyword;
+    public String content;
+    public String parentPageId;
+
+    public Page(String id, String pageName, String keyword, String content, String parentPageId) {
+        this.id = id;
+        this.pageName = pageName;
+        this.keyword = keyword;
+        this.content = content;
+        this.parentPageId = parentPageId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPageName() {
+        return pageName;
+    }
+
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getParentPageId() {
+        return parentPageId;
+    }
+
+    public void setParentPageId(String parentPageId) {
+        this.parentPageId = parentPageId;
+    }
+}
