ee0bd336a4104a9b8595a21f20d8dc344e0e6f60
QuanggCK <zaphkiel1610@gmail.com>
2026-04-01 15:14:47 +0700

    add SessionSecurity

---
+++ SessionSecurity/src/main/java/clc65/quanggck/SessionSecurityApplication.java
@@ -0,0 +1,13 @@
+package clc65.quanggck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionSecurityApplication.class, args);
+	}
+
+}
