77c677db9022d1f92113af63ff5b188529c6131e
QuanggCK <zaphkiel1610@gmail.com>
2026-04-03 13:57:08 +0700

    update AuthController

---
+++ JsonWebToken/src/main/java/clc65/quanggck/AuthController.java
@@ -0,0 +1,35 @@
+package clc65.quanggck;
+
+import clc65.quanggck.JwtResponse;
+import clc65.quanggck.LoginRequest;
+import clc65.quanggck.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public JwtResponse authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwt = jwtUtils.generateJwtToken(userDetails);
+
+        return new JwtResponse(jwt, userDetails.getUsername());
+    }
+}
