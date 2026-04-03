3e68f5d239171f62db7bcb1388c8c845fcd4deb1
QuanggCK <zaphkiel1610@gmail.com>
2026-04-03 13:44:12 +0700

    Update Repos, LoginRequest

--- JsonWebToken/src/main/java/clc65/quanggck/UserRepository.java
+++ JsonWebToken/src/main/java/clc65/quanggck/UserRepository.java
@@ -1,5 +1,11 @@
 package clc65.quanggck;
 
-public class UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import clc65.quanggck.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
 }
