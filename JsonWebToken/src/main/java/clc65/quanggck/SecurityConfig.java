1dea34b4505b521e8d06c475f7abf35c9921f5ac
QuanggCK <zaphkiel1610@gmail.com>
2026-04-03 16:26:02 +0700

    update SecutityConfig,... too much errors :<<

--- JsonWebToken/src/main/java/clc65/quanggck/SecurityConfig.java
+++ JsonWebToken/src/main/java/clc65/quanggck/SecurityConfig.java
@@ -3,7 +3,9 @@ package clc65.quanggck;
 import clc65.quanggck.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +19,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
