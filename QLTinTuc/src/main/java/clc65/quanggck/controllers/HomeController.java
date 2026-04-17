3805be4ce9a68ba72354b29d2f1ec6ec244a3b86
QuanggCK <zaphkiel1610@gmail.com>
2026-04-17 09:16:32 +0700

    update HomeController

--- QLTinTuc/src/main/java/clc65/quanggck/controllers/HomeController.java
+++ QLTinTuc/src/main/java/clc65/quanggck/controllers/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -18,6 +20,7 @@ public class HomeController {
         this.loaiTinTucService = loaiTinTucService;
     }
 
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("danhSachTin", tinTucService.getAllTinTuc());
@@ -25,9 +28,34 @@ public class HomeController {
         return "index"; 
     }
 
+    @GetMapping("/loai/{id}")
+    public String tinTucTheoLoai(@PathVariable Integer id, Model model) {
+        model.addAttribute("danhSachTin", tinTucService.getTinTucById(id)); 
+        model.addAttribute("danhSachLoai", loaiTinTucService.getAllLoaiTinTuc());
+        
+        return "index"; 
+    }
+
     @GetMapping("/tin-tuc/{id}")
     public String detail(@PathVariable Integer id, Model model) {
         model.addAttribute("tin", tinTucService.getTinTucById(id));
         return "detail"; 
+    }
+    
+    @PostMapping("/login")
+    public String xuLyDangNhap(@RequestParam("username") String username, 
+                               @RequestParam("password") String password, 
+                               Model model) {
+        if ("admin".equals(username) && "123".equals(password)) {
+            return "redirect:/admin"; 
+        } else {
+            model.addAttribute("error", true); 
+            return "login"; 
+        }
+    }
+
+    @GetMapping("/admin")
+    public String trangAdmin() {
+        return "admin"; 
     }
 }
