ac9389100156c6de20a79d64337c91d4fab39c1c
QuanggCK <zaphkiel1610@gmail.com>
2026-04-22 20:18:01 +0700

    add repo

---
+++ OnThiGK/src/main/java/thigk2/phamminhquang/repo/SanPhamRepository.java
@@ -0,0 +1,11 @@
+package thigk2.phamminhquang.repo;
+import thigk2.phamminhquang.models.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
+
+}
