915535e48b8949c4308190b37c0281261b2ef964
QuanggCK <zaphkiel1610@gmail.com>
2026-04-23 11:23:22 +0700

    lần 2

--- OnThiGK/src/main/java/thigk2/phamminhquang/models/LoaiSanPham.java
+++ OnThiGK/src/main/java/thigk2/phamminhquang/models/LoaiSanPham.java
@@ -1,5 +1,55 @@
 package thigk2.phamminhquang.models;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "loaisanpham") 
 public class LoaiSanPham {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_loai") 
+    private Integer id_loai;
+
+    @Column(name = "ten_loai", nullable = false) 
+    private String ten_loai;
+
+    @OneToMany(mappedBy = "loaiSanPham", cascade = CascadeType.ALL)
+    private List<SanPham> danhSachSanPham;
+
+    // --- CONSTRUCTORS ---
+    public LoaiSanPham() {
+    }
+
+    public LoaiSanPham(Integer id_loai, String ten_loai) {
+        this.id_loai = id_loai;
+        this.ten_loai = ten_loai;
+    }
+
+    // --- GETTERS AND SETTERS ---
+
+    public Integer getId_loai() {
+        return id_loai;
+    }
+
+    public void setId_loai(Integer id_loai) {
+        this.id_loai = id_loai;
+    }
+
+    public String getTen_loai() {
+        return ten_loai;
+    }
+
+    public void setTen_loai(String ten_loai) {
+        this.ten_loai = ten_loai;
+    }
+
+    public List<SanPham> getDanhSachSanPham() {
+        return danhSachSanPham;
+    }
+
+    public void setDanhSachSanPham(List<SanPham> danhSachSanPham) {
+        this.danhSachSanPham = danhSachSanPham;
+    }
 }
--- OnThiGK/src/main/java/thigk2/phamminhquang/models/SanPham.java
+++ OnThiGK/src/main/java/thigk2/phamminhquang/models/SanPham.java
@@ -1,5 +1,80 @@
 package thigk2.phamminhquang.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sanpham")
 public class SanPham {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_sanpham")
+    private Integer idSanPham;
+
+    @Column(name = "ten_spham", nullable = false)
+    private String tenSpham;
+
+    @Column(name = "gia", columnDefinition = "TEXT")
+    private String gia;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "id_loai", referencedColumnName = "id_loai")
+    private LoaiSanPham loaiSanPham;
+
+    // --- CONSTRUCTORS ---
+    public SanPham() {
+    }
+
+    public SanPham(Integer idSanPham, String tenSpham, String gia, String description, LoaiSanPham loaiSanPham) {
+        this.idSanPham = idSanPham;
+        this.tenSpham = tenSpham;
+        this.gia = gia;
+        this.description = description;
+        this.loaiSanPham = loaiSanPham;
+    }
+
+    // --- GETTERS AND SETTERS ---
+
+    public Integer getIdSanPham() {
+        return idSanPham;
+    }
+
+    public void setIdSanPham(Integer idSanPham) {
+        this.idSanPham = idSanPham;
+    }
+
+    public String getTenSpham() {
+        return tenSpham;
+    }
+
+    public void setTenSpham(String tenSpham) {
+        this.tenSpham = tenSpham;
+    }
+
+    public String getGia() {
+        return gia;
+    }
+
+    public void setGia(String gia) {
+        this.gia = gia;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LoaiSanPham getLoaiSanPham() {
+        return loaiSanPham;
+    }
+
+    public void setLoaiSanPham(LoaiSanPham loaiSanPham) {
+        this.loaiSanPham = loaiSanPham;
+    }
 }
