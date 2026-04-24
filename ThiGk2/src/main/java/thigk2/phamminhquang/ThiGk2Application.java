8af3d4b4a25c582c619c71a8d40b1b44df4b07d4
QuanggCK <zaphkiel1610@gmail.com>
2026-04-24 13:51:44 +0700

    commit first time

---
+++ ThiGk2/src/main/java/thigk2/phamminhquang/ThiGk2Application.java
@@ -0,0 +1,13 @@
+package thigk2.phamminhquang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiGk2Application.class, args);
+	}
+
+}
