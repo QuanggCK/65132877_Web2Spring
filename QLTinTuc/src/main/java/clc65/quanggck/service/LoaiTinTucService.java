ed67b83ad87fd6ec762bd97890363c21b739452a
QuanggCK <zaphkiel1610@gmail.com>
2026-04-24 10:48:35 +0700

    update

--- QLTinTuc/src/main/java/clc65/quanggck/service/LoaiTinTucService.java
+++ QLTinTuc/src/main/java/clc65/quanggck/service/LoaiTinTucService.java
@@ -11,30 +11,28 @@ import java.util.Optional;
 @Service
 public class LoaiTinTucService {
 
-    // Nhúng Repository vào Service (Sử dụng Constructor Injection - Cách được khuyên dùng nhất)
+
     private final LoaiTinTucRepository loaiTinTucRepository;
 
     public LoaiTinTucService(LoaiTinTucRepository loaiTinTucRepository) {
         this.loaiTinTucRepository = loaiTinTucRepository;
     }
 
-    // 1. Lấy danh sách tất cả loại tin
+
     public List<loaitintuc> getAllLoaiTinTuc() {
         return loaiTinTucRepository.findAll();
     }
 
-    // 2. Lấy một loại tin theo ID
     public loaitintuc getLoaiTinTucById(Integer id) {
         Optional<loaitintuc> optional = loaiTinTucRepository.findById(id);
-        return optional.orElse(null); // Trả về null nếu không tìm thấy
+        return optional.orElse(null); 
     }
 
-    // 3. Thêm mới hoặc Cập nhật loại tin (Spring Data JPA dùng chung hàm save)
+
     public loaitintuc saveLoaiTinTuc(loaitintuc loaiTinTuc) {
         return loaiTinTucRepository.save(loaiTinTuc);
     }
 
-    // 4. Xóa loại tin theo ID
     public void deleteLoaiTinTuc(Integer id) {
         loaiTinTucRepository.deleteById(id);
     }
